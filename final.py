fd9ec31d717625579a097170bc274d377e40d82b
Lenny lesnikowski <lennylesnikowski@gmail.com>
2022-05-19 15:48:23 -0700

    finished output

---
+++ final.py
@@ -0,0 +1,5 @@
+"""Structured English
+First create a main function that imports the text file in readme mode.
+And adds that text file to a list. And that displayed the number of grades and average grade.
+Then create a second function that calculates the percent of grades that are above the average.
+"""
