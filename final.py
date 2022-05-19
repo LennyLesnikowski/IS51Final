74c6b00478d5833c89906323319fc9c36cdfc88c
Lenny lesnikowski <lennylesnikowski@gmail.com>
2022-05-19 16:10:10 -0700

    psudocode done

--- final.py
+++ final.py
@@ -3,3 +3,26 @@ First create a main function that imports the text file in readme mode.
 And adds that text file to a list. And that displayed the number of grades and average grade.
 Then create a second function that calculates the percent of grades that are above the average.
 """
+"""
+import final file
+list = [empty]
+for every line in final file
+    append list with (int, strip line)
+
+print ("no of grades", length (list))
+
+average = (sum(list)) divided by len(list)
+
+print ("the average grade is: ", average)
+
+def calculate_percent_above_average:
+    averagescorelist = [blank]
+    %abvavg = 0
+    for var in list 
+        if var > average
+            averagescorelist.append(var)
+    %abvavg = len(averagescorelsit) / len (list) * 100
+    round (%abvavg, 2)
+    print (%abvavg)
+
+"""
