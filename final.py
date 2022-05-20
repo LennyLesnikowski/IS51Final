33ec4d11ae9ee0c632b9db485eb3df3d72440d30
Lenny lesnikowski <lennylesnikowski@gmail.com>
2022-05-19 17:07:49 -0700

    finished my code, really happy with it

--- final.py
+++ final.py
@@ -3,26 +3,47 @@ First create a main function that imports the text file in readme mode.
 And adds that text file to a list. And that displayed the number of grades and average grade.
 Then create a second function that calculates the percent of grades that are above the average.
 """
+""" Psudocode
+list = []
+define main function
+    scorelist = open file in read mode
+    for every line in list,
+        append it to the list as an integer
+define a function for calculateing the average grade
+    print = "number of grades" (len(list))
+
+    average grade  = (sum(list))/ (len(list))
+
+    print(average grade)
+    aboveAvgList = empty
+    for every variable in list
+        if variable is > average grade
+            aboveAvgList.append var
+    percentAboveAvg = len aboveAvgList / len list * 100
+    finalPercent = round the final grade
+    print (finalPercent)
+Call main()
+Call calculate_percent_above_average
 """
-import final file
-list = [empty]
-for every line in final file
-    append list with (int, strip line)
+scoreInt = []
 
-print ("no of grades", length (list))
+def main():
+    scoreList = open('final (1).txt', "r")
+    for line in scoreList:
+        scoreInt.append(int(line.strip()))
 
-average = (sum(list)) divided by len(list)
+def calculate_percent_above_average():
+    print ("number of grades: ", len(scoreInt))
 
-print ("the average grade is: ", average)
+    average = (sum(scoreInt)) / (len(scoreInt))
 
-def calculate_percent_above_average:
-    averagescorelist = [blank]
-    %abvavg = 0
-    for var in list 
-        if var > average
-            averagescorelist.append(var)
-    %abvavg = len(averagescorelsit) / len (list) * 100
-    round (%abvavg, 2)
-    print (%abvavg)
-
-"""
+    print ("Average grade: ", average)
+    aboveAverage = []
+    for var in scoreInt:
+        if var > average:
+            aboveAverage.append(var)
+    percentAboveAverage = ((len(aboveAverage)) / (len(scoreInt))) * 100
+    finalPercent = round(percentAboveAverage, 2)
+    print (finalPercent, "%")
+main()
+calculate_percent_above_average()
